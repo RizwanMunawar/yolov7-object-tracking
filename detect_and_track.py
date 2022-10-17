9a31ca4cac149823d0ca5d8d1b03cb9cd0f4cf28
Muhammad Rizwan Munawar <62513924+RizwanMunawar@users.noreply.github.com>
2022-10-17 11:16:06 +0500

    Update detect_and_track.py
    fixed minor bugs and missing packages

--- detect_and_track.py
+++ detect_and_track.py
@@ -1,11 +1,14 @@
-import argparse
-import time
-from pathlib import Path
+
+import os
 import cv2
+import time
 import torch
-import torch.backends.cudnn as cudnn
+import argparse
+from pathlib import Path
 from numpy import random
 from random import randint
+import torch.backends.cudnn as cudnn
+
 from models.experimental import attempt_load
 from utils.datasets import LoadStreams, LoadImages
 from utils.general import check_img_size, check_requirements, \
@@ -13,7 +16,8 @@ from utils.general import check_img_size, check_requirements, \
                 scale_coords, xyxy2xywh, strip_optimizer, set_logging, \
                 increment_path
 from utils.plots import plot_one_box
-from utils.torch_utils import select_device, load_classifier, time_synchronized, TracedModel
+from utils.torch_utils import select_device, load_classifier, \
+                time_synchronized, TracedModel
 from utils.download_weights import download
 
 #For SORT tracking
