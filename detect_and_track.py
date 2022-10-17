832f071ac44b7a416dd7e2c02081055e1a61f924
Muhammad Rizwan Munawar <62513924+RizwanMunawar@users.noreply.github.com>
2022-10-17 11:27:42 +0500

    Update detect_and_track.py
    fixed minor bugs

--- detect_and_track.py
+++ detect_and_track.py
@@ -278,7 +278,7 @@ if __name__ == '__main__':
     opt = parser.parse_args()
     print(opt)
     #check_requirements(exclude=('pycocotools', 'thop'))
-    if opt.download and not os.path.exists(opt.weights):
+    if opt.download and not os.path.exists(opt.weights[0]):
         print('Model weights not found. Attempting to download now...')
         download('./')
 
