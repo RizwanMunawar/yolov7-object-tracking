9d718cc798c70482458cb90ef9955ef58a6db429
Muhammad Rizwan Munawar <muhammadrizwanmunawar123@gmail.com>
2025-10-11 16:06:50 +0500

    Refactor draw_boxes function for clarity

--- detect_and_track.py
+++ detect_and_track.py
@@ -21,33 +21,33 @@ from sort import *
 
 #............................... Bounding Boxes Drawing ............................
 def draw_boxes(img, bbox, identities=None, categories=None, names=None, save_with_object_id=False, path=None,offset=(0, 0)):
-  """Function to Draw Bounding boxes"""
-  for i, box in enumerate(bbox):
-      x1, y1, x2, y2 = [int(i) for i in box]
-      x1 += offset[0]
-      x2 += offset[0]
-      y1 += offset[1]
-      y2 += offset[1]
-      cat = int(categories[i]) if categories is not None else 0
-      id = int(identities[i]) if identities is not None else 0
-      data = (int((box[0]+box[2])/2),(int((box[1]+box[3])/2)))
-      label = str(id) + ":"+ names[cat]
-      (w, h), _ = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.6, 1)
-      cv2.rectangle(img, (x1, y1), (x2, y2), (255,0,20), 2)
-      cv2.rectangle(img, (x1, y1 - 20), (x1 + w, y1), (255,144,30), -1)
-      cv2.putText(img, label, (x1, y1 - 5),cv2.FONT_HERSHEY_SIMPLEX, 
-                  0.6, [255, 255, 255], 1)
-        cv2.circle(img, data, 4, (255, 144, 30),-1)   #centroid of box
-        txt_str = ""
-        if save_with_object_id:
-            txt_str += "%i %i %f %f %f %f %f %f" % (
-                id, cat, int(box[0])/img.shape[1], int(box[1])/img.shape[0] , int(box[2])/img.shape[1], int(box[3])/img.shape[0] ,int(box[0] + (box[2] * 0.5))/img.shape[1] ,
-                int(box[1] + (
-                    box[3]* 0.5))/img.shape[0])
-            txt_str += "\n"
-            with open(path + '.txt', 'a') as f:
-                f.write(txt_str)
-    return img
+    """Function to Draw Bounding boxes"""
+    for i, box in enumerate(bbox):
+        x1, y1, x2, y2 = [int(i) for i in box]
+        x1 += offset[0]
+        x2 += offset[0]
+        y1 += offset[1]
+        y2 += offset[1]
+        cat = int(categories[i]) if categories is not None else 0
+        id = int(identities[i]) if identities is not None else 0
+        data = (int((box[0]+box[2])/2),(int((box[1]+box[3])/2)))
+        label = str(id) + ":"+ names[cat]
+        (w, h), _ = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.6, 1)
+        cv2.rectangle(img, (x1, y1), (x2, y2), (255,0,20), 2)
+        cv2.rectangle(img, (x1, y1 - 20), (x1 + w, y1), (255,144,30), -1)
+        cv2.putText(img, label, (x1, y1 - 5),cv2.FONT_HERSHEY_SIMPLEX, 
+                    0.6, [255, 255, 255], 1)
+          cv2.circle(img, data, 4, (255, 144, 30),-1)   #centroid of box
+          txt_str = ""
+          if save_with_object_id:
+              txt_str += "%i %i %f %f %f %f %f %f" % (
+                  id, cat, int(box[0])/img.shape[1], int(box[1])/img.shape[0] , int(box[2])/img.shape[1], int(box[3])/img.shape[0] ,int(box[0] + (box[2] * 0.5))/img.shape[1] ,
+                  int(box[1] + (
+                      box[3]* 0.5))/img.shape[0])
+              txt_str += "\n"
+              with open(path + '.txt', 'a') as f:
+                  f.write(txt_str)
+      return img
 #..............................................................................
 
 
